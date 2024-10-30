d07433f8aa38d91c27e069779617846150a2bb47
Sasha_Ischuk <ipz231_ios1@student.ztu.edu.ua>
2024-10-30 21:31:55 +0200

    Zminu

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,7 @@
 function getElementWidth(content, padding, border) {
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding) * 2;
-    const borderWidth = parseFloat(border) * 2;
+    const contentWidth = Number.parseFloat(content);
+    const paddingWidth = Number.parseFloat(padding) * 2;
+    const borderWidth = Number.parseFloat(border) * 2;
     return contentWidth + paddingWidth + borderWidth;
 }
 
